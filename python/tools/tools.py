31ed28c52df6adced89d3955626ddbacf8e60b62
ekenn003 <ekennedy@cern.ch>
2016-06-29 14:17:15 +0200

    move tools

--- python/tools/tools.py
+++ python/tools/tools.py
@@ -1,2 +1,10 @@
 # python/tools/tools.py
+from xsecs import xsecs
+
+def getXsec(sample):
+    if sample in xsecs:
+        return xsecs[sample]
+    else:
+        print 'No cross section info for {0}.'.format(sample)
+        return 0.
 
