8f0342aeba2f3c78e635214e970c3ee691fd233e
ekenn003 <ekennedy@cern.ch>
2016-07-12 14:07:23 +0200

    ttjets amc/mgraph

--- python/xsections.py
+++ python/xsections.py
@@ -7,32 +7,24 @@ xsecs = {
     'DoubleMuon'                                                       : 1.,
     'SingleMuon'                                                       : 1.,
 
-    # Drell-yan
+    # Drell-yan (choose one)
     'DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'          :   6025.2      * pb,
     'DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'           :   4895.       * pb * 1.216, # 1.216 LO -> NNLO
 
-    # t-tbar
-    'TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'             :     57.35     * pb,
-    'TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'    :    114.5      * pb,
-    'TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8' :    114.6      * pb,
+    # t-tbar (choose one)
     'TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'                    :    502.2      * pb,
+    'TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'                   :    831.76     * pb,
+
     # tt associated production w/boson (inclusive)
-    'TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8'          :      3.697    * pb,
     'ttZJets_13TeV_madgraphMLM'                                        :      0.259    * pb,
     'ttWJets_13TeV_madgraphMLM'                                        :      0.243    * pb,
     'ttH_M125_13TeV_powheg_pythia8'                                    :      0.5085   * pb,
     # t associated production w/boson
     'tZq_ll_4f_13TeV-amcatnlo-pythia8_TuneCUETP8M1'                    :      0.0758   * pb,
 
-    # don't use:
-    'TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8'             :      0.2529   * pb,
-
     # W (choose one)
     'WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'               :  61526.7      * pb,
     'WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'                :  61526.7      * pb,
-
-    # Zy
-    'ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'                  :    117.864    * pb,    
 
     # ZH, possible 2l from Z
     'ZH_HToBB_ZToLL_M125_13TeV_amcatnloFXFX_madspin_pythia8'           :      0.8696   * 0.5824 * (0.033658*3) * pb, # ZH * H->bb * Z->ll
@@ -41,7 +33,7 @@ xsecs = {
     'ZH_HToGG_ZToAll_M125_13TeV_powheg_pythia8'                        :      0.8696   * 0.00227               * pb, # ZH * H->gamgam
     'ZH_HToZG_ZToAll_M-125_13TeV_powheg_pythia8'                       :      0.8696   * 0.001533              * pb, # ZH * H->Zgam
     'ZHToTauTau_M125_13TeV_powheg_pythia8'                             :      0.8696   * 0.006272              * pb, # ZH * H->tautau
-    'ZH_HToZZ_4LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUgenV6_pythia8'  :      0.147    * pb, # MCM , 0.8696 * 0.02619 * (0.033658*3)**2 # ZH * H->ZZ * Z->ll^2 
+    'ZH_HToZZ_4LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUgenV6_pythia8'  :      0.147    * pb, # MCM , 0.8696 * 0.02619 * (0.033658*3)^2 # ZH * H->ZZ * Z->ll^2 
     'ZH_HToBB_ZToLL_M125_13TeV_powheg_herwigpp'                        :      0.07495  * pb,
 
     # WW
@@ -81,6 +73,12 @@ xsecs = {
     'WWZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8'                          :      0.1651   * pb,
     'WZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8'                          :      0.05565  * pb,
     'ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8'                          :      0.01398  * pb,
+
+    # don't use:
+    'TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8'             :      0.2529   * pb,
+    'TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8'          :      3.697    * pb,
+    'ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'                  :    117.864    * pb,    
     'WWG_TuneCUETP8M1_13TeV-amcatnlo-pythia8'                          :      0.2147   * pb,
     'WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8'                          :      0.04123  * pb,
+    'TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'             :     57.35     * pb,
 }
